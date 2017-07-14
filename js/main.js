9481114cf1fa8707da6cffae28f580e4a42bae69
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-07-13 20:19:53 -0400

    cambiando a materialize, arreglando inicio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 /* Holiii acá va tu código también */
+$(document).ready(function(){
+	// SideNav Options
+	$('.button-collapse').sideNav({
+  edge: 'right', // Choose the horizontal origin
+  closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+});
+
+	// Show sideNav
+$('.button-collapse').sideNav('show');
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
+
+
+	function validarPass{
+		var emailVal =  $("#email-signup").val();
+		console.log(emailValue);
+		var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+		if (caract.test(email) == false){
+			$(div).hide().removeClass('hide').slideDown('fast');
+
+			return false;
+		}else{
+			$(div).hide().addClass('hide').slideDown('slow');
+			return true;
+		}
+	}
+});
