9ba5ff690fb5bbcc2f4078bacc492112fe878e87
Valentina Miranda <valentina.mirandaj@gmail.com>
2017-07-14 11:39:13 -0400

    seccion options, questions, haciendo validaciones, estilos

--- js/main.js
+++ js/main.js
@@ -1,20 +1,74 @@
 /* Holiii acá va tu código también */
 $(document).ready(function(){
+
 	$(".button-collapse").sideNav();
 
-	$("#iniciar-sesion").click(function(event) {
-    if(!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($("#email-signup").val()))){
-   			$("#email-signup").append($("#email-signup").val("Error"));
-  		}
-  		if(!(/^\d{8}([0-9])*$/.test($("#pass-signup").val())) ){
-   			$("#pass-signup").append($("#pass-signup").val("Error"));
-  		}
-  		else{
-  			$("#iniciar-sesion").attr("href","options.html");
-        localStorage.setItem('local-mail',$("#email-signup").val());
-        localStorage.setItem('local-tarjeta',$("#pass-signup").val());
-        
-  		}
-	});
-	
+  $('.collapsible').collapsible();
+
+
+$('#validar').click(function() {
+    // Expresion regular para validar el correo
+    var regex = /[\w-\.]{2,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}/;
+
+    // Se utiliza la funcion test() nativa de JavaScript
+    if (regex.test($('#email').val().trim())) {
+        $("#iniciar-sesion").attr("href","options.html");
+    } else {
+        alert('La direccón de correo no es valida');
+    }
+});
+
+// set password variable
+        var pswd = $(this).val();
+        //validate the length
+        if ( pswd.length < 6 ) {
+            $('#length').removeClass('valid').addClass('invalid');
+        } else {
+            $('#length').removeClass('invalid').addClass('valid');
+        }
+
+        //validate number
+        if ( pswd.match(/\d/) ) {
+            $('#number').removeClass('invalid').addClass('valid');
+        } else {
+            $('#number').removeClass('valid').addClass('invalid');
+        }
+
+//mail guardado en local storage en el perfil
+  var email = localStorage.getItem("email");
+  $("#text-email").text(mail);
+
+//guarda numero de tarjetas
+  var contador = localStorage.length;
+  if (contador === null) {
+    contador = 1;
+  } 
+
+  $("#btn-agregar").on("click", function(e){
+    var numero = $("#tarjeta-input").val();
+    var regexNum = new RegExp("^([0-9]{8})$");
+
+    if(!regexNum.test(numero) || numero == ""){
+    
+       $(".malo").show();
+    }else{
+     $(".malo").hide();
+     $(".tarjetas-guardadas").append("<div class='num-area'><p id='num"+cont+"'>"+numero+"</p></div>");
+     cont++;
+     localStorage.setItem("bip"+cont, numero);
+     $("#tarjeta-input").val("");
+    }
+  });
+
+  //escribe los numeros de tarjetas al recargar
+  for(var i = 0; i < localStorage.length; i++){
+    var regexStorage = new RegExp("^([0-9]{8})$");
+    var kei = localStorage.key(i);
+    var valuee = localStorage.getItem(kei);
+    if(kei.startsWith("bip")){
+      $(".tarjetas-guardadas").append("<div class='num-area'><p id='num"+kei+"'>"+valuee+"</p></div>");
+    }
+    
+  }
+
 });
